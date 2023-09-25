01503aee6b74c98fb7c78865376135f2591e2eff
Santiago <santiago.bellido@hotmail.com>
2023-09-25 17:11:50 -0300

    METAR endpoint with error handling

---
+++ app/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const axios = require('axios');
+const { XMLParser } = require('fast-xml-parser');
+const { decode } = require('metar-decoder');
+
+const parser = new XMLParser();
+
+
+app.get('/metar', (req, res) => {
+  station = req.query.station;
+  
+  axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${station}&hoursBeforeNow=1`)
+  .then(response => {
+    const parsed = parser.parse(response.data);
+
+    if (parsed.response.data == ''){
+      res.send("Please verify your OACI code!");
+
+    } else {
+      const decoded = decode(parsed.response.data.METAR.raw_text);
+      res.send(decoded);
+    }
+
+  })
+  .catch(err => {
+    res.send("Error, please try again", err);
+  });
+
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
