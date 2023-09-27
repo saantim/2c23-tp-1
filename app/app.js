e3849dc035dcd5cf01344a651d34fca387e197b9
ovraulin <tbaldi@fi.uba.ar>
2023-09-27 18:47:34 -0300

    create spaceflight news endpoint

---
+++ app/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const axios = require('axios');
+
+app.get('/spaceflight_news', async (req, res) => {
+    const response = await axios.get(`http://api.spaceflightnewsapi.net/v3/articles?_limit=5`)
+    
+    let titles = [];
+
+    response.data.forEach(element => {
+        if(element.hasOwnProperty('title')){
+            titles.push(element.title);
+        }
+    })
+
+    res.status(200).send(titles);
+})
+
+app.listen(3000, () => console.log("Escuchando en el puerto", 3000))
