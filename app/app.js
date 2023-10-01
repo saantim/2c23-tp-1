f985d4945950c6a3b2f9238ecf25a7fd72fa9b05
Bmorseletto <bmorseletto@fi.uba.ar>
2023-10-01 10:04:08 -0300

    cache done

--- app/app.js
+++ app/app.js
@@ -5,11 +5,27 @@ const axios = require('axios');
 const { XMLParser } = require('fast-xml-parser');
 const { decode } = require('metar-decoder');
 const parser = new XMLParser();
+const redis = require('redis');
+const { setIntervalAsync, clearIntervalAsync } = require('set-interval-async');
+// usar docker run -p 6379:6379 -it redis/redis-stack-server:latest para empezar el redis 
 
-app.get('/metar', (req, res) => {
+
+const client = redis.createClient({
+  host: '127.0.0.1',
+  port: 6379
+})
+
+
+
+app.get('/metar', async (req, res) => {
   station = req.query.station;
   
-  axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${station}&hoursBeforeNow=1`)
+  clientMetar = await client.get(station)
+
+  if(clientMetar){
+    res.send(JSON.parse(clientMetar));
+  }else{
+    axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${station}&hoursBeforeNow=1`)
   .then(response => {
     const parsed = parser.parse(response.data);
 
@@ -18,6 +34,8 @@ app.get('/metar', (req, res) => {
 
     } else {
       const decoded = decode(parsed.response.data.METAR.raw_text);
+      client.set(station, JSON.stringify(decoded));
+      client.expire(station, 3600);
       res.send(decoded);
     }
 
@@ -25,6 +43,9 @@ app.get('/metar', (req, res) => {
   .catch(err => {
     res.send("Error, please try again", err);
   });
+  }
+
+  
 
 })
 
@@ -43,14 +64,42 @@ app.get('/spaceflight_news', async (req, res) => {
     const response = await axios.get(`http://api.spaceflightnewsapi.net/v3/articles?_limit=5`)
     
     let titles = [];
+  
+    clientTitles = await client.get('titles');
 
-    response.data.forEach(element => {
+    if (clientTitles){
+      titles = JSON.parse(clientTitles);  
+    }else{
+      response.data.forEach(element => {
         if(element.hasOwnProperty('title')){
             titles.push(element.title);
         }
-    })
+      })
+      clnt.set('titles', JSON.stringify(titles), (err, reply)=> {
+        if (err) console.log(err);
+
+        console.log(reply);
+      
+      
+      });
+      clnt.expire('titles', 300);
+    }
+
 
     res.status(200).send(titles);
 })
 
-app.listen(3000, () => console.log("Escuchando en el puerto", 3000))
+async function activePopulation(){
+  // Busco los datos para spaceflight apenas se abre la app
+  let titles = [];
+  const response = await axios.get(`http://api.spaceflightnewsapi.net/v3/articles?_limit=5`)
+  response.data.forEach(element => {
+    if(element.hasOwnProperty('title')){
+        titles.push(element.title);
+    }
+  })
+  clnt.set('titles', JSON.stringify(titles));
+}
+
+
+app.listen(3000, async () => {clnt = await client.connect();; console.log("Escuchando en el puerto", 3000); activePopulation();setIntervalAsync(async ()=> await activePopulation(), 60000)})
