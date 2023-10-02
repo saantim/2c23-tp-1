9a553ea60524df8b5ec8a4f141cbf34721aa3396
ovraulin <tbaldi@fi.uba.ar>
2023-10-02 19:42:49 -0300

    add hot-shot on node js app

--- app/app.js
+++ app/app.js
@@ -4,13 +4,24 @@ const port = 3000
 const axios = require('axios');
 const { XMLParser } = require('fast-xml-parser');
 const { decode } = require('metar-decoder');
+const { StatsD } = require('hot-shots');
 const parser = new XMLParser();
+
+const statsd = new StatsD({
+  host: 'graphite',
+  port: 8125,
+  prefix: 'artillery.' 
+});
 
 app.get('/metar', (req, res) => {
   station = req.query.station;
-  
+  const start = Date.now();
   axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${station}&hoursBeforeNow=1`)
   .then(response => {
+    const end = Date.now();
+    const duration = end - start;
+    statsd.timing('metar_response_time', duration);
+
     const parsed = parser.parse(response.data);
 
     if (parsed.response.data == ''){
@@ -29,8 +40,12 @@ app.get('/metar', (req, res) => {
 })
 
 app.get('/quote', async (req,res)=>{
+    const start = Date.now();
     await axios.get(`https://api.quotable.io/quotes/random`)
     .then(function (quote_response) {
+      const end = Date.now();
+      const duration = end - start;
+      statsd.timing('quote_response_time', duration);
       let quote = {"Quote": quote_response.data[0]["content"], "Author": quote_response.data[0]["author"]};
       res.status(200).send(quote);
     })
@@ -45,7 +60,11 @@ app.get('/ping', (req, res) => {
 });
 
 app.get('/spaceflight_news', async (req, res) => {
-    const response = await axios.get(`http://api.spaceflightnewsapi.net/v3/articles?_limit=5`)
+    const start = Date.now();
+    const response = await axios.get(`http://api.spaceflightnewsapi.net/v3/articles?_limit=5`);
+    const end = Date.now();
+    const duration = end - start;
+    statsd.timing('spaceflight_response_time', duration);
     
     let titles = [];
 
