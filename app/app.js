b13c7c9ba5dd001e50a2d0737e934119e8d5ba4b
Bmorseletto <bmorseletto@fi.uba.ar>
2023-10-05 12:51:05 -0300

    corecciones, tiempos

--- app/app.js
+++ app/app.js
@@ -5,6 +5,9 @@ const axios = require('axios');
 const { XMLParser } = require('fast-xml-parser');
 const { decode } = require('metar-decoder');
 const { StatsD } = require('hot-shots');
+const redis = require('redis');
+const { setIntervalAsync, clearIntervalAsync } = require('set-interval-async');
+// usar docker run -p 6379:6379 -it redis/redis-stack-server:latest para empezar el redis 
 const parser = new XMLParser();
 
 const statsd = new StatsD({
@@ -12,9 +15,7 @@ const statsd = new StatsD({
   port: 8125,
   prefix: 'artillery.' 
 });
-const redis = require('redis');
-const { setIntervalAsync, clearIntervalAsync } = require('set-interval-async');
-// usar docker run -p 6379:6379 -it redis/redis-stack-server:latest para empezar el redis 
+
 
 
 const client = redis.createClient({
@@ -31,6 +32,9 @@ app.get('/metar', async (req, res) => {
 
   if(clientMetar){
     res.send(JSON.parse(clientMetar));
+    const end = Date.now();
+    const duration = end - start;
+    statsd.timing('metar_response_time', duration);
   }else{
     axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${station}&hoursBeforeNow=1`)
   .then(response => {
@@ -46,7 +50,7 @@ app.get('/metar', async (req, res) => {
     } else {
       const decoded = decode(parsed.response.data.METAR.raw_text);
       client.set(station, JSON.stringify(decoded));
-      client.expire(station, 3600);
+      client.expire(station, 600);
       res.status(200).send(decoded);
     }
 
@@ -55,9 +59,6 @@ app.get('/metar', async (req, res) => {
     res.status(500).send(`Error: ${err}`);
   });
   }
-
-  
-
 })
 
 app.get('/quote', async (req,res)=>{
@@ -65,18 +66,9 @@ app.get('/quote', async (req,res)=>{
     client_quote = await client.get('quote');
     quote = JSON.parse(client_quote); 
     activePopulationQuote();
-    
-    await axios.get(`https://api.quotable.io/quotes/random`)
-    .then(function (quote_response) {
-      const end = Date.now();
-      const duration = end - start;
-      statsd.timing('quote_response_time', duration);
-      let quote = {"Quote": quote_response.data[0]["content"], "Author": quote_response.data[0]["author"]};
-      res.status(200).send(quote);
-    })
-    .catch(function (error){
-      res.status(500).send(`Error: ${error}`);
-    });
+    res.status(200).send(quote);
+    const end = Date.now();
+    statsd.timing('quote_response_time', duration);
     
 })
 
@@ -86,55 +78,44 @@ app.get('/ping', (req, res) => {
 
 app.get('/spaceflight_news', async (req, res) => {
     const start = Date.now();
-    const response = await axios.get(`http://api.spaceflightnewsapi.net/v3/articles?_limit=5`);
-    const end = Date.now();
+    let titles = [];
+    clientTitles = await client.get('titles');   
+    titles = JSON.parse(clientTitles);  
+    res.status(200).send(titles);
+    const end = Date.now()
     const duration = end - start;
     statsd.timing('spaceflight_response_time', duration);
-    
-    let titles = [];
-  
-    clientTitles = await client.get('titles');
-
-    if (clientTitles){
-      titles = JSON.parse(clientTitles);  
-    }else{
-      response.data.forEach(element => {
-        if(element.hasOwnProperty('title')){
-            titles.push(element.title);
-        }
-      })
-      clnt.set('titles', JSON.stringify(titles), (err, reply)=> {
-        if (err) console.log(err);
-
-        console.log(reply);
-      
-      
-      });
-      clnt.expire('titles', 300);
-    }
-
-
-    res.status(200).send(titles);
 })
 
 async function activePopulationSpaceFlight(){
   // Busco los datos para spaceflight apenas se abre la app
   let titles = [];
+  const start = Date.now();
   const response = await axios.get(`http://api.spaceflightnewsapi.net/v3/articles?_limit=5`)
+  const end = Date.now();
+  const duration = end - start;
+  statsd.timing('spaceflight_api_response_time', duration);
   response.data.forEach(element => {
     if(element.hasOwnProperty('title')){
         titles.push(element.title);
     }
   })
   clnt.set('titles', JSON.stringify(titles));
- 
-
 }
 
 async function activePopulationQuote(){
-  const quote_response = await axios.get(`https://api.quotable.io/quotes/random`);
-  let quote = {"Quote": quote_response.data[0]["content"], "Author": quote_response.data[0]["author"]};
-  clnt.set('quote', JSON.stringify(quote));
+  const start = Date.now();
+  await axios.get(`https://api.quotable.io/quotes/random`)
+    .then(function (quote_response) {
+      const end = Date.now();
+      const duration = end - start;
+      statsd.timing('quote_api_response_time', duration);
+      let quote = {"Quote": quote_response.data[0]["content"], "Author": quote_response.data[0]["author"]};
+      clnt.set('quote', JSON.stringify(quote));
+    })/*.catch(function (error){
+      res.status(500).send(`Error: ${error}`);
+    })*/;
+  
 }
 
 
@@ -144,4 +125,4 @@ app.listen(3000, async () => {
   activePopulationSpaceFlight();
   activePopulationQuote();
   setIntervalAsync(async ()=> await activePopulationSpaceFlight(), 60000);
-  setIntervalAsync(async ()=> await activePopulationQuote(), 60000); })
+ })
